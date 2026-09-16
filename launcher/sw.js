0366b80aebd6cac3ddcf6d66734e75dc4a140102
omaryazghi98-code <omaryazghi98@gmail.com>
2026-09-16 07:46:56 +0100

    launcher: add PWA service worker

---
+++ launcher/sw.js
@@ -0,0 +1,10 @@
+const CACHE='3v0l-launcher-v1';
+const ASSETS=['./dashboard.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r})));
+  }
+});
